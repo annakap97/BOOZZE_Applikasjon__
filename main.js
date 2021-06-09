bcc5870b74fea49229cec6a48b75c3460e029859
Anna Kapczuk <85514360+annakap97@users.noreply.github.com>
2021-06-09 19:09:41 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -5,15 +5,12 @@ const path = require('path')
 function createWindow () {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
-    }
+    width: 375,
+    height: 812,
   })
 
   // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
+  mainWindow.loadFile('www/index.html')
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
@@ -36,7 +33,8 @@ app.whenReady().then(() => {
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
 app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') app.quit()
+  //if (process.platform !== 'darwin') app.quit()
+  app.quit()
 })
 
 // In this file you can include the rest of your app's specific main process
